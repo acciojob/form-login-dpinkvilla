fab5666acc1ae7e03e4fdf7ff87806d368ff4e59
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-19 22:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+    let form_data=document.getElementById("form1");
 
-}
+	// function alerting() {
+	// 	f_name.value=input[0].value;
+	// 	l_name.value=input[1].value;
+	// 	return ;
+	// }
+     let f_name=form_data.getElementByTagName("input")[0].value;
+	let l_name=form_data.getElementByTagName("input")[1].value;
+
+	alert(f_name + l_name);
+};
