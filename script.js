9ed94e125f3edd9cbaf7807d67265c63fec425e4
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-03-19 22:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,35 @@ function getFormvalue() {
 	// 	l_name.value=input[1].value;
 	// 	return ;
 	// }
-     let f_name=form_data.getElementByTagName("input")[0].value;
-	let l_name=form_data.getElementByTagName("input")[1].value;
+     let f_name=form_data.getElementsByTagName("input")[0].value;
+	let l_name=form_data.getElementsByTagName("input")[1].value;
 
-	alert(f_name + l_name);
+	alert(f_name +" " + l_name);
 };
+// function getFormValue() {
+//     // Get the form element
+//     var form = document.getElementById("form1");
+
+//     // Get the values of the first and last name fields
+//     var firstName = form.elements["fname"].value;
+//     var lastName = form.elements["lname"].value;
+
+//     // Alert the first and last name
+//     alert(firstName + lastName);
+// }
+// function getFormvalue() {
+//     // Get the form element
+//     let form = document.getElementById("form1");
+
+//     // Get the values of the first and last name fields
+//     let firstNameInput = form.getElementsByTagName("input")[0].value;
+//     let lastNameInput = form.getElementsByTagName("input")[1].value;
+
+//     // Alert the first and last name
+//     alert(firstNameInput + lastNameInput);
+
+//     // Prevent the form from submitting and refreshing the page
+//     // return false;
+// }
+
+
